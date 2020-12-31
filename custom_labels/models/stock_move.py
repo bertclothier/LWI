2278b88234a45082880d2a9c7da71d410fc6565c
Ben <bmu@odoo.com>
2020-12-31 14:54:10 -0800

    custom labels module

---
+++ custom_labels/models/stock_move.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def _prepare_procurement_values(self):
+
+        values = super(StockMove, self)._prepare_procurement_values()
+        values['sale_line_id'] = self.sale_line_id.id if self.sale_line_id else False
+        return values
+    
